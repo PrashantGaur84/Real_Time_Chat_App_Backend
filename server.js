e2107b357319e70097c440d93798777675c94ca8
Prashant Gaur <prashantgaur20786@gmail.com>
2025-08-19 22:22:40 +0530

    Real Time Chat App Backend

---
+++ server.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import {Server} from 'socket.io';
+import {createServer} from 'http';
+
+const app = express();
+
+const server = createServer(app);
+
+const io = new Server(server , {
+    cors:{
+        origin:"http://localhost:5173",
+        methods:["GET" , "PUT"],
+        credentials:true,
+    }
+})
+
+io.on("connection" ,(socket)=>{
+    console.log("User Connected", socket.id);
+
+    socket.on("join_room",({name , room})=>{
+        socket.join(room);
+        console.log(name , " " , room);
+    })
+
+    socket.on("send-message" , (messageData)=>{
+        console.log(messageData);
+        socket.to(messageData.room).emit("recieve-message" , messageData);
+    })
+})
+
+const port = 3000;
+
+server.listen(port , ()=>{
+    console.log(`Servre is running on http://localhost:${port}`);
+})
